fbc11ece4e707e2c155cdbfa110ab62781ab7cae
Thu Lê <thule.240602@gmail.com>
2023-11-21 13:56:30 +0700

    CRUD post

--- src/pages/addPost.js
+++ src/pages/addPost.js
@@ -2,15 +2,24 @@ import Head from "next/head";
 import { Box } from "@mui/material";
 import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
 import { PhotoIcon } from "@heroicons/react/24/solid";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DayAdd } from "../sections/day/day-add";
+import axios from "axios";
+import { instance } from "src/api";
+import { textState } from "src/constants/constants";
+import { toast } from "react-toastify";
+import { useRecoilState } from "recoil";
+import { useRouter, withRouter } from "next/router";
+import CircularProgress from "@mui/material/CircularProgress";
 
-useEffect(() => {
-      
-},);
+
+// Define the API endpoint
 
 const PageAddPost = () => {
   const [formSections, setFormSections] = useState(1);
+  const [text, setText] = useRecoilState(textState);
+  const router = useRouter()
+  const [isSpiner, setIsSpiner] = useState(false);
 
   const addFormSection = () => {
     setFormSections((prevSections) => prevSections + 1);
@@ -22,11 +31,53 @@ const PageAddPost = () => {
     }
   };
 
+
+  const [data, setData] = useState({
+    title: "",
+    body: "",
+    images: "",
+    status: "Active",
+  });
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setData({
+      ...data,
+      [e.target.name]: value,
+    });
+  };
+  const handleAddPost = async () => {
+    const postData = {
+      title: data.title,
+      body: data.body,
+      images: data.images,
+      status: data.status,
+    };
+
+    try {
+      await instance.post("/posts", postData);
+      setText({ changeState: true });
+      setIsSpiner(true);
+    } catch (error) {
+      console.error("Error adding post:", error);
+    }
+  };
+
+  useEffect(() => {
+    if (text.changeState) {
+      toast.success('Đã thêm hoạt động thành công!');
+      setIsSpiner(false)
+      setText({ changeState: false });
+      router.push('/post')
+    }
+  }, [text]);
+
   return (
     <>
       <Head>
         <title>Post | Create</title>
       </Head>
+      {isSpiner && <CircularProgress style={spinnerStyles} />}
       <Box
         component="main"
         sx={{
@@ -42,7 +93,7 @@ const PageAddPost = () => {
               <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
                 <div className="sm:col-span-4">
                   <label
-                    htmlFor="username"
+                    htmlFor="titleActivity"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
                     Tên bài viết
@@ -51,9 +102,10 @@ const PageAddPost = () => {
                     <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
                       <input
                         type="text"
-                        name="username"
-                        id="username"
-                        autoComplete="username"
+                        id="titlePost"
+                        name="title"
+                        value={data.title}
+                        onChange={handleChange}
                         className="block flex-1 border-0 bg-transparent py-1.5 pl-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 "
                       />
                     </div>
@@ -62,22 +114,44 @@ const PageAddPost = () => {
 
                 <div className="col-span-full">
                   <label
-                    htmlFor="cover-photo"
+                    htmlFor="about"
                     className="block text-sm font-medium leading-6 text-gray-900"
                   >
-                    Thêm ảnh slide
+                    Mô tả nội dung bài viết
+                  </label>
+                  <div className="mt-2">
+                    <textarea
+                      id="about"
+                      name="body"
+                      value={data.body}
+                      onChange={handleChange}
+                      rows={3}
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                  <p className="mt-3 text-sm leading-6 text-gray-600">
+                    Viết một vài thông tin nổi bật về chuyến đi.
+                  </p>
+                </div>
+
+                <div className="col-span-full">
+                  <label
+                    htmlFor="coverThumnalil"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Thumnail image
                   </label>
                   <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
                     <div className="text-center">
                       <PhotoIcon className="mx-auto h-12 w-12 text-gray-300" aria-hidden="true" />
                       <div className="mt-4 flex text-sm leading-6 text-gray-600">
                         <label
-                          htmlFor="file-upload"
+                          htmlFor="coverThumnalil"
                           className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
                         >
                           <span>Upload a file</span>
                           <input
-                            id="file-upload"
+                            id="coverThumnalil"
                             name="file-upload"
                             type="file"
                             className="sr-only"
@@ -93,6 +167,89 @@ const PageAddPost = () => {
               </div>
             </div>
 
+            <div className=" pb-12">
+              <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                {/* <div className="sm:col-span-3">
+                  <label
+                    htmlFor="timeStart"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Ngày đi
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="date"
+                      name="timeStart"
+                      value={data.timeStart}
+                      onChange={handleChange}
+                      id="timeStart"
+                      autoComplete="given-name"
+                      className="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div> */}
+
+                {/* <div className="sm:col-span-3">
+                  <label
+                    htmlFor="timeEnd"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Ngày về
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="date"
+                      name="time_end"
+                      value={data.time_end}
+                      onChange={handleChange}
+                      id="timeEnd"
+                      autoComplete="family-name"
+                      className="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div> */}
+
+                {/* <div className="sm:col-span-4">
+                  <label
+                    htmlFor="adress"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Điểm đến
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      name="address"
+                      value={data.address}
+                      onChange={handleChange}
+                      type="text"
+                      id="adress"
+                      autoComplete="email"
+                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div> */}
+
+                {/* <div className="sm:col-span-3">
+                  <label
+                    htmlFor="sumVol"
+                    className="block text-sm font-medium leading-6 text-gray-900"
+                  >
+                    Số lượng tình nguyện viên tham gia
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      type="number"
+                      name="num_vol"
+                      id="sumVol"
+                      value={data.num_vol}
+                      onChange={handleChange}
+                      autoComplete="country-name"
+                      className="px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div> */}
+              </div>
+            </div>
           </div>
 
           <div className=" border-t border-gray-900/10 pt-10">
@@ -107,59 +264,17 @@ const PageAddPost = () => {
           {/* Chi tiết ngày */}
 
           {[...Array(formSections)].map((_, index) => (
-            <form key={index} className="max-w-4xl ml-10 mt-6">
+            <div key={index} className="max-w-4xl ml-10 mt-6">
               <h2 className="text-base font-semibold leading-7 text-gray-900">
                 Chi tiết ngày {index + 1}
               </h2>
-
-              <div className="sm:col-span-4">
-                  <label
-                    htmlFor="username"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Tên bài viết
-                  </label>
-                  <div className="mt-2">
-                    <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
-                      <input
-                        type="text"
-                        name="username"
-                        id="username"
-                        autoComplete="username"
-                        className="block flex-1 border-0 bg-transparent py-1.5 pl-4 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 "
-                      />
-                    </div>
-                  </div>
-                </div>
-
-                <div className="col-span-full">
-                  <label
-                    htmlFor="about"
-                    className="block text-sm font-medium leading-6 text-gray-900"
-                  >
-                    Mô tả đặc điểm nổi bật của hoạt động
-                  </label>
-                  <div className="mt-2">
-                    <textarea
-                      id="about"
-                      name="about"
-                      rows={3}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                      defaultValue={""}
-                    />
-                  </div>
-                  <p className="mt-3 text-sm leading-6 text-gray-600">
-                    Viết một vài thông tin nổi bật về chuyến đi.
-                  </p>
-                </div>
               <div className="mt-2">
                 <textarea
                   placeholder={`8h: Xuất phát tại Hà Nội – TP Hà Giang – Quản Bạ\n9h30: Tập trung di chuyển tại 142 Giảng Võ (Cửa hàng Kính mắt Việt Tín), Ba Đình, Hà Nội.\n20h00: Đoàn bắt đầu xuất phát.`}
-                  id="about"
+                  id="aboutDay"
                   name="about"
                   rows={3}
                   className=" px-5 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  defaultValue={""}
                 />
               </div>
               <button
@@ -170,7 +285,7 @@ const PageAddPost = () => {
                 Bỏ ngày
               </button>
               {/* ... (your existing form code) */}
-            </form>
+            </div>
           ))}
           <div className="mt-6 flex items-center gap-x-6">
             <button
@@ -187,7 +302,8 @@ const PageAddPost = () => {
               Huỷ
             </button>
             <button
-              type="submit"
+              type="button"
+              onClick={handleAddPost}
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Thêm hoạt động
@@ -199,6 +315,8 @@ const PageAddPost = () => {
   );
 };
 
-PageAddPost.getLayout = (PageAddPost) => <DashboardLayout>{PageAddPost}</DashboardLayout>;
+PageAddPost.getLayout = (PageAddPost) => (
+  <DashboardLayout>{PageAddPost}</DashboardLayout>
+);
 
 export default PageAddPost;
---
+++ src/pages/post.js
@@ -0,0 +1,121 @@
+import { useCallback, useEffect, useMemo, useState } from "react";
+import {PostTable, PostsTable} from "src/sections/post/posts-table"
+import Head from "next/head";
+import { subDays, subHours } from "date-fns";
+import ArrowDownOnSquareIcon from "@heroicons/react/24/solid/ArrowDownOnSquareIcon";
+import ArrowUpOnSquareIcon from "@heroicons/react/24/solid/ArrowUpOnSquareIcon";
+import PlusIcon from "@heroicons/react/24/solid/PlusIcon";
+import { Box, Button, Container, Stack, SvgIcon, Typography } from "@mui/material";
+import { useSelection } from "src/hooks/use-selection";
+import { Layout as DashboardLayout } from "src/layouts/dashboard/layout";
+import { ActivitiesTable } from "src/sections/activity/activities-table";
+import { ActivitiesSearch } from "src/sections/activity/activities-search";
+import { applyPagination } from "src/utils/apply-pagination";
+import axios from "axios";
+import { instance } from "src/api";
+import { textState } from "src/constants/constants";
+import { useRecoilState } from "recoil";
+import CircularProgress from "@mui/material/CircularProgress";
+import { is } from "date-fns/locale";
+import {overlayStyles, spinnerStyles, isSpiner} from '../styles/spinerStyle'
+import { PostsSearch } from "src/sections/post/posts-search";
+
+const now = new Date();
+
+const Page = () => {
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(5);
+  const [data, setData] = useState([]);
+  const [text, setText] = useRecoilState(textState);
+  const [isSpiner, setIsSpiner] = useState(false);
+
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsSpiner(true);
+
+        const response = await instance.get(
+          `/posts?page=${"1"}&pageSize=${"50"}&status=${""}`
+        );
+        console.log(response.data)
+        setData(response.data.data.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsSpiner(false);
+      }
+    };
+
+    fetchData();
+  }, [text]);
+
+  const usePosts = (page, rowsPerPage) => {
+    return useMemo(() => {
+      return applyPagination(data, page, rowsPerPage);
+    }, [page, rowsPerPage, data]);
+  };
+
+  const usePostId = (posts) => {
+    return useMemo(() => {
+      return posts.map((customer) => customer.id);
+    }, [posts]);
+  };
+
+  const handlePageChange = useCallback((event, value) => {
+    setPage(value);
+  }, []);
+
+  const handleRowsPerPageChange = useCallback((event) => {
+    setRowsPerPage(event.target.value);
+  }, []);
+
+  const posts = usePosts(page, rowsPerPage);
+  const postId = usePostId(posts);
+  const postsSelection = useSelection(postId);
+
+  return (
+    <>
+      <Head>
+        <title>Hoạt động | Green Volunteer</title>
+      </Head>
+      <div style={overlayStyles}></div>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 2,
+        }}
+      >
+        {isSpiner && <CircularProgress style={spinnerStyles} />}
+        <Container maxWidth="xl">
+          <Stack spacing={3}>
+            <Stack direction="row" justifyContent="space-between" spacing={4}>
+              <Stack spacing={1}>
+                <Typography variant="h4">Các hoạt động</Typography>
+              </Stack>
+            </Stack>
+            <PostsSearch />
+            <PostsTable
+              count={data.length}
+              items={posts}
+              onDeselectAll={postsSelection.handleDeselectAll}
+              onDeselectOne={postsSelection.handleDeselectOne}
+              onPageChange={handlePageChange}
+              onRowsPerPageChange={handleRowsPerPageChange}
+              onSelectAll={postsSelection.handleSelectAll}
+              onSelectOne={postsSelection.handleSelectOne}
+              page={page}
+              rowsPerPage={rowsPerPage}
+              selected={postsSelection.selected}
+            />
+          </Stack>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
+
+export default Page;
